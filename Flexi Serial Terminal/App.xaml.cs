6e74e86e701845bbd6ca6437bd1c4f65b6d199ee
aryan.programer <aryan.programer@gmail.com>
2020-07-08 11:33:27 +0530

    Add project files.

---
+++ Flexi Serial Terminal/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace Flexi_Serial_Terminal {
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application { }
+}
---
+++ Flexi Serial Terminal/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Flexi_Serial_Terminal {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+		}
+	}
+}
